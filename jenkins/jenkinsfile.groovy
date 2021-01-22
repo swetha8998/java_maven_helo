f8cebf1a6641f692ccfe6964641c9d8907b1abf0
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-01-22 21:40:59 +0530

    Update jenkinsfile.groovy

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -1,1 +1,16 @@
-
+node{
+    stage("checkout"){
+        checkout scm
+    }
+    stage("Build"){
+       
+        mavenPath = tool 'maven3'
+       
+        sh "${mavenPath}/mvn command"
+    }
+   
+    stage("RUN"){
+        sh "javac HelloWorld.java"
+        sh "java HelloWorld"
+    }
+}
